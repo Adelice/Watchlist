de7e2e6347f1515875c94ba19a5366ececa38ea2
Adelice <nancytoniw@gmail.com>
2019-02-26 13:17:42 +0200

    save all

--- config.py
+++ config.py
@@ -6,6 +6,14 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'  
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nancy:adelice@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 
@@ -17,6 +25,7 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+    
 
 
 class DevConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-MOVIE_API_KEY = 9e8ee2a6dc2df18bd3598ea320027c78
-SECRET_KEY = 1234567891011
-
+export MOVIE_API_KEY=9e8ee2a6dc2df18bd3598ea320027c78
+export SECRET_KEY=1234567891011
+export MAIL_USERNAME='nancytoniw@gmail.com'
+export MAIL_PASSWORD='Drdolittle1*'
 python3.6 manage.py server
